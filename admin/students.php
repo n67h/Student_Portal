1c340bb0c52ec42c333bbd70a82ddc8062382635
n67h <night67hunter@gmail.com>
2022-03-07 09:44:16 +0800

    added students.php

--- admin/students.php
+++ admin/students.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Teachers</title>
+    <title>Students</title>
 <?php
     require_once 'header.php';
     require_once 'sidebar.php';
 ?>
 <div class="content-body">
     <div>
-        <p><strong>&nbsptestsaddddddddddsssssssasdasdasdsadsadasdsadsss</strong></p>
+        <p><strong>&nbsp test test test </strong></p>
     </div>
 </div>
 
